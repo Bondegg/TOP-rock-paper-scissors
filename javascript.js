f92df7550d8f7d1e3969cd906ac8134b7402fd58
Harry Bond <harry_bond@hotmail.co.uk>
2025-07-28 22:11:48 +0100

    Add getComputerChoice logic
    Basic functionality using math.random() added to allow the copmputer
    to randomly pick rock, paper or scissors depending on math.random()
    outcome.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let num = Math.random()
+    let compChoice = null
+    if (num > 0.66) {
+        compChoice = "Rock"
+    }
+    else if (num < 0.33) {
+        compChoice = "Scissors"
+    }
+    else {
+        compChoice = "Paper"
+    }
+
+    console.log(compChoice)
+}
+
+
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
+getComputerChoice()
