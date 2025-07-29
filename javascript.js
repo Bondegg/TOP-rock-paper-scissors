9067c56138384c46f0ded42cd9a7cccddcbd7728
Harry Bond <harry_bond@hotmail.co.uk>
2025-07-29 21:51:20 +0100

    Add getHuman code, add comments
    Added prompt box for user input and basic conversions to lower case
    assuming that user input was correct. Added some basic comments on
    all code.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,33 @@
+function getHumanChoice() {
+    //Ask the user for their input via a pop up box
+    let humanChoice = prompt("Rock, Paper or Scissors? :")
+    console.log(humanChoice)
+
+    //Convert user input to lower case and update the variable with the relevant string
+    if (humanChoice.toLowerCase() === "rock") {
+        humanChoice = "rock"
+    }
+    else if (humanChoice.toLowerCase() === "paper") {
+        humanChoice = "paper"
+    }
+    else if (humanChoice.toLowerCase() === "scissors") {
+        humanChoice = "scissors"
+    }
+    else (
+        //Additional else in case the user inputs something outside of the three options, this will be used in the match logic.
+        humanChoice = null
+    )
+
+    //.log for testing purposes, code can be commented out when not in use.
+    console.log(humanChoice)
+}
+
 function getComputerChoice() {
+    //Variable declarations, num selects a random number from 0.0 -> 1.0
     let num = Math.random()
     let compChoice = null
+
+    //Assign current;y null compChoice variable with a choice depending on the random numbern in num
     if (num > 0.66) {
         compChoice = "Rock"
     }
@@ -11,16 +38,8 @@ function getComputerChoice() {
         compChoice = "Paper"
     }
 
-    console.log(compChoice)
+    //.log for testing purposes, code can be commented out when not in use.
+    //console.log(compChoice)
 }
 
-
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
-getComputerChoice()
+getHumanChoice()
